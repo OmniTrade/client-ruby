0c6a0df54c1978a2a18c9a588af23c34ac0738b7
alexandre3000 <alexandre.karpinski@codeminer42.com>
2019-01-28 17:18:28 -0200

    remove unused else statement from client.rb

--- lib/omnitrade_api/client.rb
+++ lib/omnitrade_api/client.rb
@@ -67,13 +67,11 @@ module OmniTradeAPI
     end
 
     def setup_auth_keys(options)
-      if options[:access_key] && options[:secret_key]
-        @access_key = options[:access_key]
-        @secret_key = options[:secret_key]
-        @auth       = Auth.new @access_key, @secret_key
-      else
-        #raise ArgumentError, 'Missing access key and/or secret key'
-      end
+      return unless options[:access_key] and options[:secret_key]
+
+      @access_key = options[:access_key]
+      @secret_key = options[:secret_key]
+      @auth       = Auth.new @access_key, @secret_key
     end
 
     def check_auth!
